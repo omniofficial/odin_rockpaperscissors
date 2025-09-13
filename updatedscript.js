05d5af5c587c366a4d02aef2273de953d944600b
omniofficial <omniofficial04@gmail.com>
2025-09-12 17:01:15 -0700

    Refactored functions to display in DOM

--- updatedscript.js
+++ updatedscript.js
@@ -11,86 +11,102 @@ scissorsbtn.addEventListener("click", () => playGame(3));
 // Initialization of variables to keep track of scores
 let humanScore = 0;
 let computerScore = 0;
+let roundsPlayed = 0;
 
 // Play game logic
 function playGame(humanSelection) {
+    roundsPlayed++;
     const computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-    displayScores();
+    displayComputerChoice(computerSelection);
+    playRound(humanSelection, computerSelection, roundsPlayed);
+    displayScores(roundsPlayed);
 }
 
 // CHANGE INTO DOM. Function to acquire the computer choice. Returns INT.
 function getComputerChoice() {
     let choice;
     choice = Math.floor(Math.random() * 3) + 1;
-
-    if (choice == 1) {
-        console.log("The computer has chosen ROCK!");
-    } else if (choice == 2) {
-        console.log("The computer has chosen PAPER!");
-    } else if (choice == 3) {
-        console.log("The computer has chosen SCISSORS!");
-    }
     return choice;
 }
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice == 1 && computerChoice == 1) {
-        console.log(
-            "Looks like you tied! There is no winner. Scores will be kept the same"
-        );
-    } else if (humanChoice == 1 && computerChoice == 2) {
-        console.log(
-            "You lose! Paper beats rock. The computer score will increase."
-        );
-        computerScore++;
-    } else if (humanChoice == 1 && computerChoice == 3) {
-        console.log(
-            "You win! Rock beats scissors. The human score will increase."
-        );
-        humanScore++;
-    } else if (humanChoice == 2 && computerChoice == 1) {
-        console.log(
-            "You win! Paper beats rock. The human score will increase."
-        );
-        humanScore++;
-    } else if (humanChoice == 2 && computerChoice == 2) {
-        console.log(
-            "Looks like you tied! There is no winner. Scores will be kept the same"
-        );
-    } else if (humanChoice == 2 && computerChoice == 3) {
-        console.log(
-            "You lose! Scissors beats paper. The computer score will increase."
-        );
-        computerScore++;
-    } else if (humanChoice == 3 && computerChoice == 1) {
-        console.log(
-            "You lose! Rock beats scissors. The computer score will increase."
-        );
-        computerScore++;
-    } else if (humanChoice == 3 && computerChoice == 2) {
-        console.log(
-            "You win! Scissors beats paper. The human score will increase."
-        );
-        humanScore++;
-    } else if (humanChoice == 3 && computerChoice == 3) {
-        console.log(
-            "Looks like you tied! There is no winner. Scores will be kept the same"
-        );
+function displayComputerChoice(choice) {
+    const computerChoiceDisplay = document.querySelector("#computer-choice");
+
+    if (choice == 1) {
+        computerChoiceDisplay.textContent = "Computer has chosen ROCK!";
+    }
+    if (choice == 2) {
+        computerChoiceDisplay.textContent = "Computer has chosen PAPER!";
+    }
+    if (choice == 3) {
+        computerChoiceDisplay.textContent = "Computer has chosen SCISSORS!";
     }
 }
 
-function displayScores() {
+function playRound(humanChoice, computerChoice, roundsPlayed) {
+    const gameContainer = document.querySelector("#play-round");
+
+    // Create the round header only if it doesn't exist yet
+    let roundHeader = gameContainer.querySelector("h2");
+    if (!roundHeader) {
+        roundHeader = document.createElement("h2");
+        roundHeader.textContent = `Round ${roundsPlayed} analysis`;
+        gameContainer.appendChild(roundHeader);
+    } else {
+        // Update the header with the current round number
+        roundHeader.textContent = `Round ${roundsPlayed} analysis`;
+    }
+
+    // Find or create the message paragraph
+    let contextParagraph = gameContainer.querySelector("p");
+    if (!contextParagraph) {
+        contextParagraph = document.createElement("p");
+        gameContainer.appendChild(contextParagraph);
+    }
+
+    // Paragraph context logic
+    if (humanChoice == 1 && computerChoice == 1) {
+        message =
+            "Looks like you tied! There is no winner. Scores will be kept the same";
+    } else if (humanChoice == 1 && computerChoice == 2) {
+        message =
+            "You lose! Paper beats rock. The computer score will increase.";
+        computerScore++;
+    } else if (humanChoice == 1 && computerChoice == 3) {
+        message =
+            "You win! Rock beats scissors. The human score will increase.";
+        humanScore++;
+    } else if (humanChoice == 2 && computerChoice == 1) {
+        message = "You win! Paper beats rock. The human score will increase.";
+        humanScore++;
+    } else if (humanChoice == 2 && computerChoice == 2) {
+        message =
+            "Looks like you tied! There is no winner. Scores will be kept the same";
+    } else if (humanChoice == 2 && computerChoice == 3) {
+        message =
+            "You lose! Scissors beats paper. The computer score will increase.";
+        computerScore++;
+    } else if (humanChoice == 3 && computerChoice == 1) {
+        message =
+            "You lose! Rock beats scissors. The computer score will increase.";
+        computerScore++;
+    } else if (humanChoice == 3 && computerChoice == 2) {
+        message =
+            "You win! Scissors beats paper. The human score will increase.";
+        humanScore++;
+    } else if (humanChoice == 3 && computerChoice == 3) {
+        message =
+            "Looks like you tied! There is no winner. Scores will be kept the same";
+    }
+    contextParagraph.textContent = message;
+    // Add a class here to message to style it
+    gameContainer.appendChild(contextParagraph);
+}
+
+function displayScores(roundsPlayed) {
     console.log("------------- CURRENT SCORES ------------- ");
     console.log("Human Score: " + humanScore);
     console.log("Computer Score: " + computerScore);
-}
-
-let rounds = 0;
-
-for (let i = 1; i <= rounds; i++) {
-    console.log("------------- GAME: " + i + " -------------");
-    playGame();
 }
 
 if (humanScore < computerScore) {
